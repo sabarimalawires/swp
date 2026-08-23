faf7f356bf7adfb9b5458acfe441f661aba26dd6
sabarimalawires <sabarimalawires@gmail.com>
2026-08-23 21:27:18 +0530

    Use same-origin Better Auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { usernameClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   plugins: [
     usernameClient({
       displayUsername: false,
